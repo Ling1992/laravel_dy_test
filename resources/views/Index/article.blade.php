7f18d99cb3d093d099f592c158ef325aa23bd9b4
Ling1992 <l_ing1992@163.com>
2017-11-01 11:15:17 +0800

    add base file =x=x

--- resources/views/Index/article.blade.php
+++ resources/views/Index/article.blade.php
@@ -37,6 +37,31 @@
             </div>
             <p class="blog-post-meta">57大收录 ---------------------------------- </p>
         </div>
+        <div>
+            <p style="text-align: center;">
+                <span style="color: rgb(84, 141, 212);">资源找不到？ 资源链接失效？ </span>
+            </p>
+            <p style="text-align: center;">
+                <span style="color: rgb(84, 141, 212);">那就加入我们 57大收录 交流群吧 ！！</span>
+            </p>
+            <p style="text-align: center;">
+                <span style="color: rgb(84, 141, 212);">资源互享 当然你们还能结识志同道合的伙伴 哦！！</span>
+            </p>
+            <p style="text-align: center;">
+                <img src="{{ asset('img/qun.JPG') }}" title="qun.jpg" alt="qun.JPG" style="width: 60%"/>
+            </p>
+        </div>
+        <div>
+            <p style="text-align: center;">
+                <span style="color: rgb(84, 141, 212);">扫描二维码关注 57大收录，做老司机，防迷路， </span>
+            </p>
+            <p style="text-align: center;">
+                <span style="color: rgb(84, 141, 212);">查看更多电影信息，回复片名，火速获取</span>
+            </p>
+            <p style="text-align: center;">
+                <img src="{{ asset('img/qrcode_for_gh_3f4c8072f490_1280.jpg') }}" title="qrcode_for_gh_3f4c8072f490_1280.jpg" alt="qrcode_for_gh_3f4c8072f490_1280.JPG" style="width: 60%"/>
+            </p>
+        </div>
     </div><!-- /.blog-post -->
 
 @endsection
